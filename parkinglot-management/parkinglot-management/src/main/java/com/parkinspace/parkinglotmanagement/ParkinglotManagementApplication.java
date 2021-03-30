fe8c77dbec6438dfa3eb7ea464eb67356258a48c
Andres Ardila <andres.ardila.agudelo@gmail.com>
2021-03-29 21:06:57 -0500

    mvn sb intital

---
+++ parkinglot-management/parkinglot-management/src/main/java/com/parkinspace/parkinglotmanagement/ParkinglotManagementApplication.java
@@ -0,0 +1,13 @@
+package com.parkinspace.parkinglotmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotManagementApplication.class, args);
+	}
+
+}
---
+++ parkinglot-management/parkinglot-management/src/test/java/com/parkinspace/parkinglotmanagement/ParkinglotManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkinspace.parkinglotmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
