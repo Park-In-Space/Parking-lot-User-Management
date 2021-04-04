6619df08e6a8cdac34aceb3b8ac3ae82f6c87326
AndresArdila544 <arardilaag@unal.edu.co>
2021-04-04 03:04:25 -0500

    docker ready

--- parkinglot-management/parkinglot-management/src/main/java/com/parkinspace/parkinglotmanagement/ParkinglotManagementApplication.java
+++ parkinglot-management/parkinglot-management/src/main/java/com/parkinspace/parkinglotmanagement/ParkinglotManagementApplication.java
@@ -3,6 +3,11 @@ package com.parkinspace.parkinglotmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+sudo docker run --network='host' -p 8080:8080 -e DB_HOST=127.0.0.1 -e DB_PORT=3306 -e DB_USER=admin -e DB_PASSWORD=password -e DB_NAME=parkinglotuser -e URL=0.0.0.0:8080 parkinglot-management:0.0.2-SNAPSHOT
+*/
+
+
 @SpringBootApplication
 public class ParkinglotManagementApplication {
 
